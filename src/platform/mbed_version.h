ff0643f817852f8f5623ae11d2936786d2a8e07d
Thomas Kemmer <tkemmer@computer.org>
2018-11-20 20:26:49 +0100

    Rebase on mbed-lib 164.

--- src/platform/mbed_version.h
+++ src/platform/mbed_version.h
@@ -24,27 +24,43 @@
 #ifndef MBED_VERSION_H
 #define MBED_VERSION_H
 
-#define MBED_LIBRARY_VERSION 163
-
 /** MBED_MAJOR_VERSION
-  * Mbed 2 major version
+  * Mbed OS major version
+  *
+  * @note 99 is default value for development version (master branch)
   */
-#define MBED_MAJOR_VERSION 2
+#define MBED_MAJOR_VERSION 5
 
 /** MBED_MINOR_VERSION
-  * Mbed 2 minor version
+  * Mbed OS minor version
+  *
+  * @note 99 is default value for development version (master branch)
   */
-#define MBED_MINOR_VERSION 0
+#define MBED_MINOR_VERSION 10
 
 /** MBED_PATCH_VERSION
-  * Mbed 2 patch version
+  * Mbed OS patch version
+  *
+  * @note 99 is default value for development version (master branch)
   */
-#define MBED_PATCH_VERSION 163
+#define MBED_PATCH_VERSION 3
 
 #define MBED_ENCODE_VERSION(major, minor, patch) ((major)*10000 + (minor)*100 + (patch))
 
+/** MBED_VERSION
+  * Mbed OS 5 version (MMmmpp - M(Major); m(minor); p(patch))
+  *
+  * @note 999999 is default value for development version (master branch)
+  */
 #define MBED_VERSION MBED_ENCODE_VERSION(MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION)
 
+/** MBED_VERSION_CHECK
+  * Macro can be used to check minimum Mbed OS version required for feature/library. If current version
+  * is less than required, it will assert.
+  *
+  * @note: Version of master branch will be 999999 as default, hence no assert/warning is provided for
+  * master branch code
+  */
 #define MBED_VERSION_CHECK(major, minor, patch) do { \
                                        MBED_STATIC_ASSERT((MBED_VERSION >= MBED_ENCODE_VERSION((major),(minor),(patch))), "Incompatible mbed-os version detected!!"); \
                                      } while(0)
